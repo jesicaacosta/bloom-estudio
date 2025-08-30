dc16e9a7015366bb530e0e16bf1ba6ee1c5f32fa
jesicaacosta <ajesicadaiana@gmail.com>
2025-08-30 19:19:23 -0300

    Cambios en deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], //voy a trabajar con react
+    base: '/bloom-estudio/', // ruta base del proyecto para github pages
 })
